c741e9ff021131f3b9ef1807e785790f70d7c6d6
Vivek Mishra <vivekm@covacsis.com>
2016-11-08 20:12:58 +0530

    Feature scaling

--- DataPreprocessing/data_processing_template.R
+++ DataPreprocessing/data_processing_template.R
@@ -30,3 +30,7 @@ set.seed(123)
 split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 training_set =  subset(dataset, split == TRUE)
 test_set =  subset(dataset, split == FALSE)
+
+# Feature scaling
+training_set[, 2:3] = scale(training_set[, 2:3])
+test_set[, 2:3] = scale(test_set[, 2:3])
--- DataPreprocessing/data_processing_template.py
+++ DataPreprocessing/data_processing_template.py
@@ -46,3 +46,10 @@ Y = labelencoder_Y.fit_transform(Y)
 # Splitting the dataset into Training set and test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+# sc_X is already fit in training set so no need to fit for test set
+X_test = sc_X.transform(X_test)
