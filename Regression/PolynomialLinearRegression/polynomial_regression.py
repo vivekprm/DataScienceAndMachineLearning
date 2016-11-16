c4ec755c45ff93bc0fad99ca3aa616391722735f
Vivek Mishra <vivekm@covacsis.com>
2016-11-16 14:53:39 +0530

    Prediction bluff or truth

--- Regression/PolynomialLinearRegression/polynomial_regression.py
+++ Regression/PolynomialLinearRegression/polynomial_regression.py
@@ -46,21 +46,26 @@ plt.ylabel('Salary')
 plt.show()
 
 # Visualizing the Polynomial Regression results.
-#plt.scatter(X, Y, color = 'red')
-#plt.plot(X, lin_reg2.predict(poly_reg.fit_transform(X)), color = 'blue')
-#plt.title('Truth or Bluff (Polynomial Regression)')
-#plt.xlabel('Position Level')
-#plt.ylabel('Salary')
-#plt.show()
-
-# More real plot
-X_grid = np.arange(min(X), max(X), 0.1)
-# To make above vector array
-X_grid = X_grid.reshape(len(X_grid), 1)
 plt.scatter(X, Y, color = 'red')
-plt.plot(X_grid, lin_reg2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.plot(X, lin_reg2.predict(poly_reg.fit_transform(X)), color = 'blue')
 plt.title('Truth or Bluff (Polynomial Regression)')
 plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
 
+# More real plot
+#X_grid = np.arange(min(X), max(X), 0.1)
+# To make above vector array
+#X_grid = X_grid.reshape(len(X_grid), 1)
+#plt.scatter(X, Y, color = 'red')
+#plt.plot(X_grid, lin_reg2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+#plt.title('Truth or Bluff (Polynomial Regression)')
+#plt.xlabel('Position Level')
+#plt.ylabel('Salary')
+#plt.show()
+
+# Predicting new result with Linear Regression
+lin_reg.predict(6.5)
+
+# Predicting new result with Polynomial Regression
+lin_reg2.predict(poly_reg.fit_transform(6.5))
